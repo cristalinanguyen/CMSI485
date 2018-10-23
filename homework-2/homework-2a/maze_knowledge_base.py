b755ce98e8dff79ceecd15ea09ec3f810dbb196f
Cristalina Nguyen <cristalinanguyen@me.com>
2018-10-22 18:28:19 -0700

    Completed

--- homework-2/homework-2a/maze_knowledge_base.py
+++ homework-2/homework-2a/maze_knowledge_base.py
@@ -7,45 +7,57 @@ with side-information.
 '''
 from maze_clause import MazeClause
 import unittest
+import itertools
 
 class MazeKnowledgeBase:
-    
+
     def __init__ (self):
         self.clauses = set()
-    
+
     def tell (self, clause):
         """
         Adds the given clause to the CNF MazeKnowledgeBase
         Note: we expect that no clause added this way will ever
         make the KB inconsistent (you need not check for this)
         """
-        # TODO: This is currently implemented incorrectly; see
-        # spec for details!
-        return
-        
+        self.clauses.add(clause)
+
     def ask (self, query):
         """
         Given a MazeClause query, returns True if the KB entails
         the query, False otherwise
         """
-        # TODO: Implement resolution inference here!
-        # This is currently implemented incorrectly; see
-        # spec for details!
-        return False
-            
-    
+        clausez = set(self.clauses)
+        new = set()
+
+        # Where p is the proposition and v is the truth value assigned
+        for p, v in query.props.items():
+            clausez.add(MazeClause([(p, not v)]))
+
+        while True:
+            for each_pair in list(itertools.combinations(clausez, 2)):
+                resolved = MazeClause.resolve(each_pair[0], each_pair[1])
+                for pair in resolved:
+                    if pair.is_empty():
+                        return True
+                new = new | resolved
+            # If clausez contains new then we stop
+            if new <= clausez:
+                return False
+            clausez = clausez | new
+
 class MazeKnowledgeBaseTests(unittest.TestCase):
     def test_mazekb1(self):
         kb = MazeKnowledgeBase()
         kb.tell(MazeClause([(("X", (1, 1)), True)]))
         self.assertTrue(kb.ask(MazeClause([(("X", (1, 1)), True)])))
-        
+
     def test_mazekb2(self):
         kb = MazeKnowledgeBase()
         kb.tell(MazeClause([(("X", (1, 1)), False)]))
         kb.tell(MazeClause([(("X", (1, 1)), True), (("Y", (1, 1)), True)]))
         self.assertTrue(kb.ask(MazeClause([(("Y", (1, 1)), True)])))
-        
+
     def test_mazekb3(self):
         kb = MazeKnowledgeBase()
         kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), True)]))
@@ -54,7 +66,7 @@ class MazeKnowledgeBaseTests(unittest.TestCase):
         kb.tell(MazeClause([(("X", (1, 1)), True)]))
         self.assertTrue(kb.ask(MazeClause([(("W", (1, 1)), True)])))
         self.assertFalse(kb.ask(MazeClause([(("Y", (1, 1)), False)])))
-        
+
     def test_mazekb4(self):
         kb = MazeKnowledgeBase()
         kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), True), (("W", (1, 1)), True)]))
@@ -64,7 +76,7 @@ class MazeKnowledgeBaseTests(unittest.TestCase):
         kb.tell(MazeClause([(("W", (1, 1)), True)]))
         kb.tell(MazeClause([(("T", (1, 1)), True)]))
         self.assertTrue(kb.ask(MazeClause([(("Z", (1, 1)), False)])))
-        
+
     def test_mazekb5(self):
         kb = MazeKnowledgeBase()
         kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), True), (("W", (1, 1)), True)]))
@@ -74,7 +86,7 @@ class MazeKnowledgeBaseTests(unittest.TestCase):
         kb.tell(MazeClause([(("W", (1, 1)), True)]))
         kb.tell(MazeClause([(("T", (1, 1)), True)]))
         self.assertTrue(kb.ask(MazeClause([(("Z", (1, 1)), True), (("W", (1, 1)), True)])))
-    
+
     def test_mazekb6(self):
         kb = MazeKnowledgeBase()
         kb.tell(MazeClause([(("X", (1, 1)), False), (("Y", (1, 1)), False), (("Z", (1, 1)), False)]))
@@ -82,8 +94,7 @@ class MazeKnowledgeBaseTests(unittest.TestCase):
         self.assertFalse(kb.ask(MazeClause([(("Z", (1, 1)), False)])))
         kb.tell(MazeClause([(("Y", (1, 1)), True)]))
         self.assertTrue(kb.ask(MazeClause([(("Z", (1, 1)), False)])))
-        
-        
+
+
 if __name__ == "__main__":
     unittest.main()
-    
